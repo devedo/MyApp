cf9f30c8a570d71d60d0a9026541499ed0ec5854
josh <j>
2020-12-13 19:02:46 +0100

    start

---
+++ src/test/java/com/spring/test/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
